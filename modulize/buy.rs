fd994e14b6e9cb7eacb1ae006a113a118257b780
YusungAN <52990109+YusungAN@users.noreply.github.com>
2022-07-16 15:25:28 +0900

    Update buy.rs

--- modulize/buy.rs
+++ modulize/buy.rs
@@ -4,6 +4,8 @@ pub fn get_item_info(name: String, item_list: &[Item]) {
     for i in item_list {
         if name.eq(&i.name) {
             println!("name {}price: {}", i.name, i.price);
+            return;
         }
     }
+    println!("item does not exist.");
 }
