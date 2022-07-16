df6aad1aec82877c260d22b797306cedc18571b8
YusungAN <52990109+YusungAN@users.noreply.github.com>
2022-07-16 15:31:16 +0900

    Update sale.rs

--- modulize/sale.rs
+++ modulize/sale.rs
@@ -1,6 +1,13 @@
 use super::Item;
 
 pub fn register_item(name: String, price: usize, product_cost: usize, item_list: &mut Vec<Item>) {
+    for i in item_list.as_slice() {
+        if name.eq(&i.name) {
+            println!("already exist item.");
+            return;
+        }
+    }
+    
     let item = Item {
         name: name,
         price: price,
